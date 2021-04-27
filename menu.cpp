a82ef8f2aed8dfe0796fcb425a2ce7477aacae7a
Николай Заболотнов <z.nik.vladim.312@gmail.com>
2021-04-27 16:46:44 +0300

    Added menu constructor and destructor realisation

--- menu.cpp
+++ menu.cpp
@@ -1,6 +1,40 @@
 #include "menu.hpp"
 
+#include <iostream>
+
 menu::menu()
 {
+    char n; //
+    while(true)
+    {
+        std::cout <<
+        "1)Открыть файл\n"
+        "2)Сохранить файл\n"
+        "3)Изменение записи\n"
+        "4)Поиск\n"
+        "5)Вывод списка на экран\n"
+        "6)О программе\n"
+        "q)Вывод" << std::endl;
 
+        std::cin >> n;
+        switch (n)
+        {
+            case 49:  break;
+            case 50:  break;
+            case 51:  break;
+            case 52:  break;
+            case 53:  break;
+            case 54: std::cout << "Домашняя работа №3, Заболотнов Николай Владимирович "
+                                   "ИЭУИС-2-6 \n" << std::endl;
+                      break;
+            case 113: exit(EXIT_SUCCESS);
+
+            default: std::cerr << "Error: wrond menu command" << std::endl;
+            }
+        }
+}
+
+menu::~menu()
+{
+    system("cls");
 }
--- menu.hpp
+++ menu.hpp
@@ -5,11 +5,15 @@
 class menu
 {
 public:
-    menu(); //конструктор меню по умолчанию
+    menu(); //конструктор меню по умолчанию, создает меню и отрисовывает его
     ~menu(); //деструктор меню по умолчанию
 
     menu(const menu &other) = delete; //запрет создания конструктора копирования меню
 
+    //методы класса меню
+    void openFile();
+    void saveFile();
+    void displayList();
 };
 
 #endif // MENU_HPP
